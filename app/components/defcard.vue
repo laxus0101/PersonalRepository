56aed587d60130e8233f93b113feba7640551fa2
Emmanuel Gaona Acevedo <emm.gaona@gmail.com>
2025-12-03 18:03:52 -0600

    ajuste de estilos en defcard y optimización de layout en index.vue para mejor responsividad

--- app/components/defcard.vue
+++ app/components/defcard.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="sm:max-w-sm p-6 bg-white border border-gray-200 dark:bg-gray-800 dark:border-gray-700 rounded-lg shadow-sm">
+<div class="p-6 bg-white border border-gray-200 dark:bg-gray-800 dark:border-gray-700 rounded-lg shadow-sm">
     <a href="#">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{props.titulo}}</h5>
     </a>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,21 +1,27 @@
 <template>
-  <div id="mainbody" class="py-24 sm:py-4">
-    <div class="mx-auto max-w-7xl px-6 lg:px-8 mb-8 border border-red-500">
-      <div class="mx-auto px-10 lg:mx-0 grid grid-cols-2 grid-rows-2 gap-0 items center">
+  <div id="mainbody" class="py-4">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8 mb-8">
+      <div class="mx-auto px-1 sm:px-10 lg:mx-0 grid grid-cols-2 grid-rows-2 gap-0 items center">
         <h2
-          class="self-center sm:text-6xl sm:text-white-400 text-4xl font-semibold tracking-tight text-dark dark:text-white">
+          class="self-center text-3xl sm:text-5xl md:text-6xl text-dark dark:text-white font-semibold tracking-tight">
           Emmanuel Gaona</h2>
         <p
           class="mt-5 text-xl font-medium text-pretty text-gray-600 dark:text-gray-100 sm:text-xl/8 col-start-1 row-start-2">
           Hola, soy Emmanuel 👋
           <br>Desarrollador Web Frontend
         </p>
-        <div id="ContenedorImagen" class="flex justify-center items-center row-span-2 col-start-2 row-start-1">
-          <div id="ImgPerfil">
+        <div id="ContenedorImagen" class="flex justify-center items-center row-span-2 col-start-2 row-start-1 mt-2">
+          <div id="ImgPerfil"
+            class="w-[300px] h-[300px] sm:w-[400px] sm:h-[400px] rounded-full"
+            style="
+              background-image: url('/img/profilepic.jpg');
+              background-size: cover;
+              background-position: center;"
+            >
           </div>
         </div>
       </div>
-      <div class="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none border border-red-500 mb-5">
+      <div class="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none mb-5">
         <p class="mb-3 text-gray-600 dark:text-gray-200 text-xl"> "Soy un desarrollador web especializado en frontend
           con
           experiencia en <strong class="font-bold text-gray-900 dark:text-white"> Nuxt</strong>, y actualmente
@@ -24,10 +30,10 @@
         </p>
       </div>
     </div>
-    <div class="flex flex-wrap justify-center mx-auto max-w-7xl px-6 lg:px-8 mb-8 border border-red-500 gap-2">
-      <Defcard v-for="tarjeta in contenidoTrajetas" :titulo=tarjeta.titulo :descripcion=tarjeta.descripcion />
+    <div class="flex flex-wrap gap-2 mx-auto max-w-7xl px-6 lg:px-8 mb-8">
+      <Defcard class="grow" v-for="tarjeta in contenidoTrajetas" :titulo=tarjeta.titulo :descripcion=tarjeta.descripcion />
     </div>
-    <div class="flex flex-wrap justify-center mx-auto max-w-7xl px-6 lg:px-8 mb-8 border border-red-500">
+    <div class="flex flex-wrap justify-center mx-auto max-w-7xl px-6 lg:px-8 mb-8">
 
     </div>
   </div>
@@ -38,26 +44,26 @@ import Defcard from '~/components/defcard.vue';
 const contenidoTrajetas = ref([
   {
     titulo: 'Tarjeta 1',
-    descripcion: 'Descripcion de la tarjeta 1'
+    descripcion: 'Descripcion de la tarjeta 1Descripcion de la tarjeta 1Descripcion de la tarjeta 1Descripcion de la tarjeta 1Descripcion de la tarjeta 1'
   },
   {
     titulo: 'Tarjeta 2',
-    descripcion: 'Descripcion de la tarjeta 2'
+    descripcion: 'Descripcion de la tarjeta 2Descripcion de la tarjeta 1Descripcion de la tarjeta 1Descripcion de la tarjeta 1Descripcion de la tarjeta 1Descripcion de la tarjeta 1'
   },
   {
     titulo: 'Tarjeta 3',
-    descripcion: 'Descripcion de la tarjeta 3'
+    descripcion: 'Descripcion de la tarjeta 3Descripcion de la tarjeta 1Descripcion de la tarjeta 1Descripcion de la tarjeta 1Descripcion de la tarjeta 1Descripcion de la tarjeta 1'
   }
 ]);
 </script>
 
 <style>
-#ContenedorImagen #ImgPerfil {
+/* #ContenedorImagen #ImgPerfil {
   width: 400px;
   height: 400px;
   background-image: url("/img/profilepic.jpg");
   background-size: cover;
   background-position: center;
   border-radius: 50%;
-}
+} */
 </style>
