bc2cdc16fbb528fad130676adc434de7a8ecf2d7
Emmanuel Gaona Acevedo <emm.gaona@gmail.com>
2025-12-09 19:07:57 -0600

    refactor layout and styles in app.vue, carousel.vue, and index.vue for improved responsiveness and organization

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="grid grid-rows-[auto 1fr auto] min-h-screen bg-cyan-100 dark:bg-gray-800">
-    <header class="flex justify-end content-center pe-14 py-1 sticky top-0 bg-cyan-200 dark:bg-gray-700 z-10">
+  <div class="grid grid-rows-[auto 1fr auto] min-h-screen bg-cyan-100 dark:bg-gray-800 overflow-x-hidden">
+    <header class="flex justify-end content-center pe-14 py-1 fixed w-full top-0 bg-cyan-200 dark:bg-gray-700 z-10">
       <button @click.prevent="toggleDarkmode()" id="theme-toggle" data-tooltip-target="tooltip-toggle" type="button" class="text-gray-500 inline-flex items-center justify-center dark:text-gray-400 w-10 h-10 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 border border-gray-300 dark:border-gray-600">
         <svg id="theme-toggle-dark-icon" class="hidden w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
           <path d="M17.8 13.75a1 1 0 0 0-.859-.5A7.488 7.488 0 0 1 10.52 2a1 1 0 0 0 0-.969A1.035 1.035 0 0 0 9.687.5h-.113a9.5 9.5 0 1 0 8.222 14.247 1 1 0 0 0 .004-.997Z"></path>
--- app/components/carousel.vue
+++ app/components/carousel.vue
@@ -1,10 +1,15 @@
 <template>
-    <div class="w-[70%]">
-        <Marquee :reverse="true" :fade="true">
-            <div v-for="(card, index) in content" :key="index" class="flex flex-col items-center bg-neutral-primary-soft px-4 py-1 border border-default rounded-base shadow-xs md:max-w-xl dark:border-gray-700 dark:bg-gray-800 mx-2">
-                <span>{{ card.img }}</span>
-                <div class="flex flex-col justify-between md:p-4 leading-normal">
-                    <h5 class="text-xl font-bold tracking-tight text-heading dark:text-white">{{ card.text }}</h5>
+    <div class="w-[70%] py-1">
+        <Marquee
+            class="gap-[1rem]"
+            :reverse="true"
+            :fade="true"
+            :duration="20"
+            >
+            <div v-for="(card, index) in content" :key="index" class="flex flex-col items-center bg-neutral-primary-soft px-4 my-1 border border-default rounded-base md:max-w-xl dark:border-gray-700 dark:bg-gray-800 mx-4 shadow-2xl shadow-gray-400/10">
+                <img :src=card.img alt="python logo" class="w-6 h-6 md:w-8 md:h-8 mt-2"/>
+                <div class="flex flex-col justify-between px-4 py-1 leading-normal">
+                    <h5 class="text-md font-bold tracking-tight text-heading dark:text-white">{{ card.text }}</h5>
                     <div>
                     </div>
                 </div>
@@ -15,15 +20,45 @@
 
 <script setup>
 const content = [
-    { text: 'JavaScript', img: '🟨' },
-    { text: 'Python', img: '🐍' },
-    { text: 'C#', img: '🎮' },
-    { text: 'Nuxt', img: '🖥️' },
-    { text: 'Tailwind', img: '🎨' },
-    { text: 'Flowbite', img: '🧩' },
-    { text: 'Bootstrap', img: '📚' },
-    { text: 'React', img: '⚛️' },
-    { text: 'HTML', img: '🌐' },
-    { text: 'CSS', img: '🎨' }
+    {
+        text: 'JavaScript',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png'
+    },
+    {
+        text: 'Python',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg'
+    },
+    {
+        text: 'C#',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/4/4f/Csharp_Logo.png'
+    },
+    {
+        text: 'Nuxt',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/a/ae/Nuxt_logo.svg'
+    },
+    {
+        text: 'Tailwind',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg'
+    },
+    {
+        text: 'Flowbite',
+        img: 'https://flowbite.com/docs/images/logo.svg'
+    },
+    {
+        text: 'Bootstrap',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Bootstrap_logo.svg'
+    },
+    {
+        text: 'React',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg'
+    },
+    {
+        text: 'HTML',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg'
+    },
+    {
+        text: 'CSS',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg'
+    }
 ];
 </script>
